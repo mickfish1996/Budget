136d16a4c9b8c821ccc4e4fe630003108e7115bd
mickfish1996 <fis17005@byui.edu>
2022-01-11 13:06:37 -0700

    updated

--- Main.java
+++ Main.java
@@ -1,21 +1,45 @@
 package Budget;
+import java.util.Scanner;
 
 public class Main {
     
-    /* This method will display all the text that the program is needing
-       to display. It will use the formating that it is sent. */
+    /******************************************************************************** 
+     * This method will display all the text that the program is needing
+     * to display. It will use the formating that it is sent. 
+     ********************************************************************************/
     static void displayText(String text){
         System.out.println(text);
     }
+    /******************************************************************************** 
+     * User Input will Prompt the user for the number that they would like to enter.
+     * If they enter the wrong number than it will prompt again  
+     ********************************************************************************/
+    static int userInput(){
+        int userIn = 0;
+        try{
+            Scanner myObj = new Scanner(System.in);
+            System.out.print("Enter a number: ");
 
-    /* Main will run the program, when it needs to it will call the other methods
-       that are in the class. It will also provide a menue that the user can select
-       what they want to do with the program.*/
+            userIn = myObj.nextInt();
+            
+        }
+
+        catch(Exception e){
+            System.out.println("Please enter a number between 1 and 3");
+        }
+        return userIn;
+    }
+    /******************************************************************************** 
+     * Main will run the program, when it needs to it will call the other methods
+     * that are in the class. It will also provide a menue that the user can select
+     * what they want to do with the program.
+     ********************************************************************************/
     public static void main(String[] args){
         displayText("\nBudgeting Program");
         boolean keepGoing = true; // Used in the while loop
         while (keepGoing){
         displayText("\t1. View Balance\n\t2. Add Spending\n\t3. Exit\n");
+        int input = userInput();
         keepGoing = false; // updates to make the loop stop.
         }
     }
