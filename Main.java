51e0942aea3c2f98db0ae03bc566fe55a4ecc64c
mickfish1996 <fis17005@byui.edu>
2022-01-11 12:18:41 -0700

    added to main

--- Main.java
+++ Main.java
@@ -2,4 +2,11 @@ package Budget;
 
 public class Main {
     
+    static void displayText(String text){
+        System.out.println(text);
+    }
+    public static void main(String[] args){
+        displayText("\nBudgeting Program");
+        displayText("\t1. View Balance\n\t2. Add Spending\n\t3. Exit\n");
+    }
 }
