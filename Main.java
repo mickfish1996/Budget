767a78c7dc44f573e37e73d70e8322906898b029
mickfish1996 <fis17005@byui.edu>
2022-01-11 12:27:57 -0700

    updated

--- Main.java
+++ Main.java
@@ -7,6 +7,11 @@ public class Main {
     }
     public static void main(String[] args){
         displayText("\nBudgeting Program");
+        boolean keepGoing = true;
+        while (keepGoing){
+        
         displayText("\t1. View Balance\n\t2. Add Spending\n\t3. Exit\n");
+        keepGoing = false;
+        }
     }
 }
